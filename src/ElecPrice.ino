6ed5fffd54fef1d4c3da746d24dd6a6b528f6308
Mathias Berglund Mathiesen <you@example.com>
2022-09-27 08:29:30 +0200

    Changed to a statemachine. Tried sleep - not applicable

--- src/ElecPrice.ino
+++ src/ElecPrice.ino
@@ -3,14 +3,16 @@
 #include "../lib/MQTT/src/MQTT.h"
 
 #define DELTA_OFFSET 0.3
-#define KW_SENSOR_PIN D6
+#define KW_SENSOR_PIN D8
 #define WATT_CONVERSION_CONSTANT 3600000
 #define HOST "192.168.0.103"
 #define PORT 1883
 #define SECONDS_TO_DAY 86400
-#define PULL_TIME_1 23*3600 + 10*60
-#define PULL_TIME_2 11*3600 + 10*60
+#define PULL_TIME_1 23
+#define PULL_TIME_2 11
 #define MAX_RANGE 48
+#define MAX_TRANSMIT_BUFF 128
+#define SLEEP_DURATION 30000
 
 int oneShotGuard = -1;
 double cost[MAX_RANGE];
@@ -18,7 +20,6 @@ int cost_hour[MAX_RANGE];
 int date;
 int range = MAX_RANGE;         // Max received count. Updated if received count is smaller
 char temp[5*513];       // Create an array that can hold the entire transmission
-byte rec_cnt;           // Counter to keep track of recieved transmissions
 bool populate = false;  // Entire transmission received flag
 bool work = false;      // Received price data
 bool printer = false;
@@ -35,6 +36,20 @@ int calc_power;
 // Send updated meassurement flag
 bool transmit_value = false;
 
+enum statemachine
+{
+    SENSOR_READ = 0,
+    GET_DATA = 1,
+    CALCULATE = 2,
+    TRANSMIT_PRICE = 3,
+    TRANSMIT_SENSOR = 4,
+    SLEEP_STATE = 5,
+    AWAITING_DATA = 6,
+    STARTUP = 10,
+};
+
+statemachine state = GET_DATA;
+
 void calc_low(void);
 void get_data(int day);
 void handle_sensor(void);
@@ -47,8 +62,26 @@ MQTT client("192.168.0.103", PORT, 512, 30, callback);
 // Create sleep instance
 SystemSleepConfiguration config;
 
+SYSTEM_THREAD(ENABLED);
+//SYSTEM_MODE(MANUAL);
+
 void setup()
 {
+    //Particle.connect();
+    pinMode(SENSOR_READ, OUTPUT);
+    pinMode(GET_DATA, OUTPUT);
+    pinMode(CALCULATE, OUTPUT);
+    pinMode(TRANSMIT_PRICE, OUTPUT);
+    pinMode(TRANSMIT_SENSOR, OUTPUT);
+    pinMode(SLEEP_STATE, OUTPUT);
+    pinMode(AWAITING_DATA, OUTPUT);
+    pinMode(STARTUP, OUTPUT);
+
+    digitalWrite(STARTUP, HIGH);
+    waitUntil(Particle.connected);
+    digitalWrite(STARTUP, LOW);
+
+    digitalWrite(state, HIGH);
     last_read = millis(); //Give it an initial value
     pinMode(KW_SENSOR_PIN, INPUT_PULLDOWN);                 //Setup pinmode for LDR pin
     attachInterrupt(KW_SENSOR_PIN,handle_sensor,RISING);    //Attach interrup that will be called when rising
@@ -58,8 +91,8 @@ void setup()
     Particle.subscribe("get_prices", myPriceHandler, MY_DEVICES);
 
     // Request data on power prices for the next 48 hours
-    get_data(Time.day());
-
+    //get_data(Time.day());
+    Particle.variable("State",state);
 
     // connect to the server(unique id by Time.now())
     Serial.printf("Return value: %d",client.connect("sparkclient_" + String(Time.now()),"mqtt","mqtt"));
@@ -75,7 +108,7 @@ void setup()
     }
 
     // Setup low power mode
-    config.mode(SystemSleepMode::ULTRA_LOW_POWER).gpio(KW_SENSOR_PIN, RISING).network(NETWORK_INTERFACE_ALL);
+    //config.mode(SystemSleepMode::ULTRA_LOW_POWER).gpio(KW_SENSOR_PIN, RISING).network(NETWORK_INTERFACE_ALL);
 }
 
 void callback(char* topic, byte* payload, unsigned int length) 
@@ -91,22 +124,30 @@ void callback(char* topic, byte* payload, unsigned int length)
 
 void handle_sensor(void)
 {
+
+    //statemachine prev_state = state;
     unsigned long delta;
     unsigned long current_reading = millis();
-    
+
     if ((delta = current_reading-last_read) > 100)
     {
+        Serial.printf("In interrupt\n");
+        digitalWrite(state, LOW);
+        state = SENSOR_READ;
+        digitalWrite(state, HIGH);
         calc_power = WATT_CONVERSION_CONSTANT / delta;
         last_read = current_reading;
-        printer = true; // Just a debuging flag
         transmit_value = true;
+        digitalWrite(state, LOW);
+        state = TRANSMIT_SENSOR;
+        //state = prev_state;
+        digitalWrite(state, HIGH);
     }
 }
 
 void myHandler(const char *event, const char *data)
 {
     populate = false;
-    rec_cnt++;
 
     /* When transmissions are greater than 512 bytes, it will be split into 512
      * byte parts. The final transmission part should therefore be less than 512.
@@ -148,16 +189,35 @@ void myHandler(const char *event, const char *data)
                 break;      // Break the while loop
             }
         }
+        digitalWrite(state, LOW);
+        state = CALCULATE;
+        digitalWrite(state, HIGH);
     }
 }
 
 void myPriceHandler(const char *event, const char *data)
 {
     update_prices = true;
+    digitalWrite(state, LOW);
+    state = GET_DATA;
+    digitalWrite(state, HIGH);
 }
 
 void loop()
 {
+    // Note: Change to be hour based so device can sleep longer
+    // wakeup on interrupt but go back to sleep again.
+    // Check what time it is
+    int currentHour = Time.hour();
+    if (( currentHour == PULL_TIME_1 || currentHour == PULL_TIME_2) && currentHour != oneShotGuard)
+    {
+        oneShotGuard = currentHour;
+        update_prices = true;
+        digitalWrite(state, LOW);
+        state = GET_DATA;
+        digitalWrite(state, HIGH);
+    }
+    
     if (client.isConnected())
     {
         client.loop();
@@ -166,37 +226,32 @@ void loop()
     {
         Serial.printf("Client disconnected\n");
         reconnect();
-    }
-
-    // Check what time it is
-    int currentSecond = Time.local() % SECONDS_TO_DAY;
-    if ((currentSecond == PULL_TIME_1 || currentSecond == PULL_TIME_2) && currentSecond != oneShotGuard)
-    {
-        oneShotGuard = currentSecond;
-        update_prices = true;
-    }
-
-    // Is it time to update the prices or has it been requested?
-    if (update_prices == true)
-    {
-        get_data(Time.day());
-        update_prices = false;
+        if (client.isConnected())
+        {
+            Serial.printf("Client reconnected\n");
+        }
     }
     
-    if (printer) // Debugging flag set in interrupt handler
+
+    // Is it time to update the prices or has it been requested?
+    if (state == GET_DATA)
     {
-        Serial.printf("Light: %d\n",calc_power);
-        printer = false;
+        digitalWrite(state, LOW);
+        state = AWAITING_DATA;
+        digitalWrite(state, HIGH);
+        get_data(Time.day());
     }
+    
     // Has the prices for today arrived?
-    if (populate)
+    if (state == CALCULATE)
     {
         calc_low();
         Serial.printf("Current HH:MM: %02d:%02d\n", Time.hour() + 2, Time.minute());
     }
 
-    if (work)
+    if (state == TRANSMIT_PRICE)
     {
+        
         Serial.printf("In work\n");
         // Do some meaningfull work with the collected data
         String data = "Cheap(ish) hours of the day: ";
@@ -208,22 +263,68 @@ void loop()
         Particle.publish("Low price hours", data, PRIVATE);
         // Publish cheap hour to MQTT
         client.publish("prices", data);
+        client.loop();
         work = false;
+        digitalWrite(state, LOW);
+        state = SLEEP_STATE;
+        digitalWrite(state, HIGH);
+        
     }
 
-    if (transmit_value) // Did we receive a request for updated values
+    if (state == TRANSMIT_SENSOR) // Did we receive a request for updated values
     {
         Serial.printf("Received power/get\n");
         char values[16];
         sprintf(values,"%d", calc_power);
         client.publish("power",values);
         transmit_value = false;
+        digitalWrite(state, LOW);
+        state = SLEEP_STATE;
+        digitalWrite(state, HIGH);
     }
     // Wait 1 second
     delay(1000);
-    //System.sleep(config);
+
+    /*
+    // Get current millis()
+    system_tick_t current_reading = millis();
+    // Dynamic sleep duration 
+    system_tick_t elapsed_sleep;
+    if (state == SLEEP_STATE)
+    {
+        
+        while ((elapsed_sleep = millis() - current_reading) < SLEEP_DURATION)
+        {
+            client.disconnect();
+            Particle.disconnect();
+            WiFi.off();
+
+            Serial.printf("In sleep loop: %ld\n",SLEEP_DURATION-elapsed_sleep);
+            config.mode(SystemSleepMode::ULTRA_LOW_POWER).duration(SLEEP_DURATION-elapsed_sleep).gpio(KW_SENSOR_PIN, RISING);
+            System.sleep(config);
+        }
+        /*
+        if (Particle.connected())
+        {
+            Particle.process();
+        }
+        */
+        // Roughly 10 seconds have passed!
+        /*
+        digitalWrite(state, LOW);
+        state = TRANSMIT_SENSOR;
+        digitalWrite(state, HIGH);
+        WiFi.on();
+        WiFi.connect();
+        waitUntil(WiFi.ready);
+        Particle.connect();
+        Particle.process();
+    }
+    */
 }
 
+/** @brief Reconnects MQTT client if disconnected
+ */
 void reconnect(void)
 {
     client.connect("sparkclient_" + String(Time.now()),"mqtt","mqtt");
@@ -272,8 +373,6 @@ void calc_low(void)
         }
     }
 
-    // Calculations have been done - clear flag
-    populate = false;
     // Display the results
     Serial.printf("Highest price of the day: %f\n", last_big);
     Serial.printf("Lowest price of the day: %f\n", last_small);
@@ -303,7 +402,11 @@ void calc_low(void)
         Serial.printf("%02d to %02d\n",start_stop[z][0],start_stop[z][1]);
     }
 
+    // Calculations are done - set state
     work = true;
+    digitalWrite(state, LOW);
+    state = TRANSMIT_PRICE;
+    digitalWrite(state, HIGH);
 }
 
 /** @brief Puplishes a formatted command string to Particle cloud that fires off a webhook
@@ -311,8 +414,7 @@ void calc_low(void)
  */
 void get_data(int day)
 {
-    rec_cnt = 0;
-    range = 48;
+    range = MAX_RANGE;
     cnt = 0;
     temp[0] = 0;
     String data = String::format("{ \"year\": \"%d\", \"month\":\"%02d\", \"day\": \"%02d\", \"day_two\": \"%02d\", \"hour\": \"%02d\" }", Time.year(), Time.month(), day, day + 2, Time.hour());
